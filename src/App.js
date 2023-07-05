d93d4f1aedf2e4e58240533026bcc4cde482e4bb
asifmalik-ba <asif.salman.malik@b-architects.com>
2023-07-05 18:00:43 +0900

    Update Code with Dynamic Component

--- src/App.js
+++ src/App.js
@@ -7,16 +7,18 @@ function MyButton() {
   );
 }
 
-function ProfileCard(){
+function ProfileCard({ name, designation }){
   return (
+    <div className="col-4">
     <div className="card">
       <img src="https://ca.slack-edge.com/T024FLV32-U01C098LKJQ-a5a97d31e847-192" class="card-img-top" alt="..." />
       <div className="card-body">
-        <h5 className="card-title">アシフ・マリク</h5>
-        <span>ITエンジニア</span>
+        <h5 className="card-title">{name}</h5>
+        <span>{designation}</span>
         <p className="card-text"></p>
         <a href="#" className="btn btn-primary">プロファイルを表示</a>
       </div>
+    </div>
     </div>
   );
 }
@@ -25,7 +27,11 @@ function App() {
   return (
     <div className='container'>
       <h1 className='text-center mt-5'>開発者の自己紹介</h1>
-      <ProfileCard />
+      <div className="row">
+      <ProfileCard name="アシフ・マリク" designation="IT エンジニア" />
+      <ProfileCard name="Asif Malik" designation="IT Engineer" />
+      </div>
+      
     </div>
   );
 }
