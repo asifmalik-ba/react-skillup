f5d1cd9fa8feb40ab334ef00367de06baf7a9177
asifmalik-ba <asif.salman.malik@b-architects.com>
2023-07-05 17:55:03 +0900

    Update project with Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+function MyButton() {
+  return (
+    <button className="btn btn-primary">I'm a button</button>
+  );
+}
+
+function ProfileCard(){
+  return (
+    <div className="card">
+      <img src="https://ca.slack-edge.com/T024FLV32-U01C098LKJQ-a5a97d31e847-192" class="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">アシフ・マリク</h5>
+        <span>ITエンジニア</span>
+        <p className="card-text"></p>
+        <a href="#" className="btn btn-primary">プロファイルを表示</a>
+      </div>
+    </div>
+  );
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1 className='text-center mt-5'>開発者の自己紹介</h1>
+      <ProfileCard />
     </div>
   );
 }
